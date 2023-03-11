372c29fdc0c0a8ec7f5a220a7436ac38c9e8f901
Mario <tihanymario@gmail.com>
2023-03-11 06:54:33 +0000

    added searchview categoryselect

---
+++ src/views/SearchView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import CategorySelect from '../components/CategorySelect.vue';
+</script>
+
+<template>
+    <div class="p-8">
+        <button type="button" class="text-white text-2xl font-semibold hover:underline cursor-pointer">
+            Recommended Radios
+        </button>
+        <div class="py-1.5">
+            <div class="flex items-center">
+                <CategorySelect category="Podcasts" image="https://picsum.photos/id/40/300/300" />
+            </div>
+        </div>
+    </div>
+</template>
